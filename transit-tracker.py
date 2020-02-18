e9d7ce1f34066fd33ad5cbd120218a2eaa3df6b3
adam-lim1 <adam.lim.1@gmail.com>
2020-02-17 20:07:07 -0600

    black HMTL formatting / dynamic route styling / bus info

--- transit-tracker.py
+++ transit-tracker.py
@@ -7,28 +7,57 @@ app = Flask(__name__)
 
 config = configparser.RawConfigParser()
 config.read('config.txt')
-key = config.get('Credentials', 'key')
+train_key = config.get('Credentials', 'train_key')
+bus_key = config.get('Credentials', 'bus_key')
+
+
+def getTrainTime(input_str):
+    """
+    Return number of minutes until train is due
+    """
+    time_diff = datetime.datetime.strptime(input_str, "%Y-%m-%dT%H:%M:%S") - datetime.datetime.now()
+    return round(time_diff.seconds / 60)
+
+def getRouteClass(rt):
+    """
+    Dynamically define CSS class named based on Train route
+    """
+    if rt == 'P':
+        return "transit-rectangle-purple"
+    elif rt == 'Brn':
+        return "transit-rectangle-Brn"
+    else:
+        return "transit-rectangle-bus"
+
+def getBusTime(input_str):
+    """
+    Return number of minutes until bus is due
+    """
+    time_diff = datetime.datetime.strptime(input_str, "%Y%m%d %H:%M") - datetime.datetime.now()
+    return round(time_diff.seconds / 60)
 
 @app.route('/')
 def tracker_page():
 
-    # Sedgwick (All)
-    mapid = '40800' # 30156 (South)
-    endpoint = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={key}&mapid={mapid}&outputType=JSON".format(key=key, mapid=mapid)
-    response = requests.get(endpoint).json()#['ctatt']['eta']
+    ####### TRAIN INFO #######
+    mapid = '40800' # Sedgwick (All trains)
+    endpoint = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={key}&mapid={mapid}&outputType=JSON".format(key=train_key, mapid=mapid)
+    response = requests.get(endpoint).json() #['ctatt']['eta']
 
-    def getTime(input_str):
-        time_diff = datetime.datetime.strptime(input_str, "%Y-%m-%dT%H:%M:%S") - datetime.datetime.now()
-        return round(time_diff.seconds / 60)
+    train_info = [(x['rt'], x['destNm'], getTrainTime(x['arrT']),getRouteClass(x['rt'])) for x in response['ctatt']['eta']][0:5] # First 4 entries
 
-    def getRouteClass(rt):
-        if rt == 'P':
-            return "transit-rectangle-purple"
-        elif rt == 'Brn':
-            return "transit-rectangle-Brn"
-        else:
-            return "transit-rectangle-bus"
+    ####### BUS INFO #######
 
-    train_info = [(x['rt'], x['destNm'], getTime(x['arrT']),getRouteClass(x['rt'])) for x in response['ctatt']['eta']][0:5]
+    stpid = 927 # North Ave / Sedgwick (Northeast Corner)
+    rt = 72
+    endpoint = "http://www.ctabustracker.com/bustime/api/v2/getpredictions?key={key}&stpid={stpid}&rt={rt}&format=json".format(key=bus_key, stpid=stpid, rt=rt)
+    response = requests.get(endpoint)
+    response = response.json()['bustime-response']['prd']
+
+    bus_info = [(x['des'], getBusTime(x['prdtm'])) for x in response[0:2]]
+
+    ####### RETURN INFO #######
+
     return render_template('tracker.html',
-                            train_list=train_info)
+                            train_list=train_info,
+                            bus_list=bus_info)
